3e3aac86eeebb5b75a22a937892ba92cfe8161e1
nimrod <nimrodw@kubiyax.com>
2025-12-03 15:13:45 +0200

    refactor: remove unused BarChart and PieChart components from the UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,6 @@ import { Box, Container } from '@mui/material';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './stores/store';
-import { AppPieChart } from './ui/PieChart';
-import { AppBarChart } from './ui/BarChart';
 import { AppLineChart } from './ui/LineChart';
 
 export const App: React.FC = () => {
